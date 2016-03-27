99f6f9265ccd0c1f214e515a8166822a6e5d0e46
zhonghua <zhonghua.cd.tuan800.com>
2016-03-27 19:43:04 +0800

    remove space

--- config/loaders/controllers.lua
+++ config/loaders/controllers.lua
@@ -1,7 +1,7 @@
 local controllers = {}
 local files = io.popen('ls ' .. app.root .. 'app/controllers/*.lua')
 
-for controller_path in files:lines() do 
+for controller_path in files:lines() do
   controller_name = controller_path:match('app/controllers/(.+).lua')
   require_path = controller_path:match('(.+).lua')
   controllers[controller_name] = require(require_path)
